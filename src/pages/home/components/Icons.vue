36e5b6dca571ee0b91c13fee8003aceed63fc240
Daiyousei9 <lvjiana_1@163.com>
2022-06-09 10:46:37 +0800

    推荐区域样式逻辑

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -75,36 +75,38 @@ export default {
     overflow hidden
     height 0
     padding-bottom 50%
-  .icon
-    position relative
-    overflow hidden
-    float left
-    width 25%
-    height 0
-    padding-bottom 25%
-    .icon-img
-      position absolute
-      top 0
-      left 0
-      right 0
-      bottom .44rem
-      box-sizing border-box
-      padding 0.1rem
-      .icon-img-content
-        display block
-        margin 0 auto
-        height 100%
-    .icon-desc
-      position absolute
-      left 0
-      right 0
-      bottom 0
-      padding 0 10%
-      line-height .44rem
-      height .44rem
-      text-align center
-      font-size .2rem
-      margin 0
-      color $darkTextColor
-      ellipse()
+  .icons
+    margin-top .1rem
+    .icon
+      position relative
+      overflow hidden
+      float left
+      width 25%
+      height 0
+      padding-bottom 25%
+      .icon-img
+        position absolute
+        top 0
+        left 0
+        right 0
+        bottom .44rem
+        box-sizing border-box
+        padding 0.1rem
+        .icon-img-content
+          display block
+          margin 0 auto
+          height 100%
+      .icon-desc
+        position absolute
+        left 0
+        right 0
+        bottom 0
+        padding 0 10%
+        line-height .44rem
+        height .44rem
+        text-align center
+        font-size .2rem
+        margin 0
+        color $darkTextColor
+        ellipse()
 </style>
---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item in recommendedList " :key="item.id">
+        <img class="item-img" :src="item.imgUrl" />
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-btn">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendedList: [
+        {
+          id: '0001',
+          imgUrl: 'https://tr-osdcp.qunarzz.com/tr-osd-tr-space/img/45a4e09567253f0051fa23c0ea9215d1.jpg_640x480x70_65b36d72.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首站，狼浪漫的海洋主题乐园'
+        },
+        {
+          id: '0002',
+          imgUrl: 'https://tr-osdcp.qunarzz.com/tr-osd-tr-space/img/45a4e09567253f0051fa23c0ea9215d1.jpg_640x480x70_65b36d72.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首站，狼浪漫的海洋主题乐园'
+        },
+        {
+          id: '0003',
+          imgUrl: 'https://tr-osdcp.qunarzz.com/tr-osd-tr-space/img/45a4e09567253f0051fa23c0ea9215d1.jpg_640x480x70_65b36d72.jpg',
+          title: '大连圣亚海洋世界',
+          desc: '浪漫大连首站，狼浪漫的海洋主题乐园'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import "~%styles/mixins.styl"
+  .title
+    margin-top .1rem
+    line-height .8rem
+    font-size .32rem
+    background #eee
+    text-indent .2rem
+  .item
+    overflow hidden
+    display flex
+    height 1.9rem
+    .item-img
+      width 1.7rem
+      height 1.7rem
+      padding .1rem
+    .item-info
+      flex 1
+      padding .1rem
+      min-width 0
+      .item-title
+        line-height .54rem
+        font-size .32rem
+        ellipse()
+      .item-desc
+        line-height: .4rem;
+        color #ccc
+        ellipse()
+      .item-btn
+        line-height .44rem
+        margin-top .16rem
+        background #ff9300
+        padding 0 0.2rem
+        border-radius .06rem
+        color #fff
+</style>
